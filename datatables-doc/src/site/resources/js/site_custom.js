1a8628c8e7316d96f9010e50123136299490e23e
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-03-05 13:29:06 +0100

    Updated doc site
    New version
    Added YourKit testimonial

--- datatables-doc/src/site/resources/js/site_custom.js
+++ datatables-doc/src/site/resources/js/site_custom.js
@@ -1,6 +1,6 @@
 // Javascript executed in all pages
 $(document).ready(function () {
-	$('footer div.row').html($('footer div.row').html() + "<a href='./main.acknowledgments.html'>Acknowledgment</a>");
+	$('footer div.row').html($('footer div.row').html() + "<a href='./acknowledgments.html'>Acknowledgment</a>");
 	$('#twitter').css('margin-top', '10px');
 	$('#poweredBy').append('<a href="http://www.cloudbees.com/" class="poweredBy"><img src="./images/logo_cloudbees_built-on.png" width="100px" height="40px" /></a><div class="clear"></div>');
 	$('#poweredBy').css('text-align', 'center');
