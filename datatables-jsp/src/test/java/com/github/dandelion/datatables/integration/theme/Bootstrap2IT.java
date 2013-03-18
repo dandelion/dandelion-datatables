fd443946bc570e6dc77735f083a75c56d717211b
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-03-18 13:39:41 +0100

    New Bootstrap2 IT

---
+++ datatables-jsp/src/test/java/com/github/dandelion/datatables/integration/theme/Bootstrap2IT.java
@@ -0,0 +1,72 @@
+/*
+ * [The "BSD licence"]
+ * Copyright (c) 2013 Dandelion
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of Dandelion nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.github.dandelion.datatables.integration.theme;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.github.dandelion.datatables.integration.DomBaseIT;
+
+/**
+ * Test the Bootstrap2 theme feature.
+ *
+ * @author Thibault Duchateau
+ */
+public class Bootstrap2IT extends DomBaseIT {
+
+	@Test
+	public void should_generate_table_markup() throws IOException, Exception {
+		goTo("/themes/bootstrap2_default.jsp");
+	
+		StringBuffer baseHref = new StringBuffer("http://");
+		baseHref.append(SERVER_HOST);
+		baseHref.append(":");
+		baseHref.append(SERVER_PORT);
+		baseHref.append("/datatablesController/datatables-bootstrap2.css?id=");
+		baseHref.append(TABLE_ID);
+		baseHref.append("&c=http%3A%2F%2F");
+		baseHref.append(SERVER_HOST);
+		baseHref.append("%3A");
+		baseHref.append(SERVER_PORT);
+		baseHref.append("%2Fthemes%2Fbootstrap2_default.jsp");
+		
+		// Custom Bootstrap CSS must exist
+		assertThat(getHtmlBody().findFirst("link").getAttribute("href")).isEqualTo(baseHref.toString());
+		
+		// Looking for the paging div
+		assertThat(find("#" + TABLE_ID + "_wrapper").find("div.paging_bootstrap ")).hasSize(1);
+	}
+	
+	// TODO test export links generation
+}
---
+++ datatables-jsp/src/test/webapp/themes/bootstrap2_default.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ include file="../common/taglib.jsp"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<%@ include file="../common/head.jsp"%>
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+   <datatables:table id="myTableId" data="${persons}" theme="bootstrap2">
+      <datatables:column title="Id" property="id" />
+      <datatables:column title="FirstName" property="firstName" />
+      <datatables:column title="LastName" property="lastName" />
+      <datatables:column title="City" property="address.town.name" />
+      <datatables:column title="Mail" property="mail" />
+   </datatables:table>
+</body>
+</html>
