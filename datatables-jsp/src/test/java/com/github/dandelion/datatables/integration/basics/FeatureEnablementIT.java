9f3be233c44a629f60ad446de938325e5dc8152e
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-04-04 19:52:24 +0200

    Tests for JSP implementation now use the testing-jsp dependency

--- datatables-jsp/src/test/java/com/github/dandelion/datatables/integration/basics/FeatureEnablementIT.java
+++ datatables-jsp/src/test/java/com/github/dandelion/datatables/integration/basics/FeatureEnablementIT.java
@@ -37,7 +37,7 @@ import java.io.IOException;
 import org.junit.Test;
 
 import com.github.dandelion.datatables.integration.DomBaseIT;
-import com.github.dandelion.datatables.utils.Mock;
+import com.github.dandelion.datatables.testing.core.Mock;
 
 /**
  * Test the basic Features of Dandelion-Datatables.
