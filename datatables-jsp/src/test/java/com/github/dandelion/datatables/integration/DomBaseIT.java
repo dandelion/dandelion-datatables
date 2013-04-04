8f6cde4e4c2f8fe82b0e2c3e799db437e77688e0
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-04-04 21:07:07 +0200

    New Thymeleaf IT for multiple tables

--- datatables-jsp/src/test/java/com/github/dandelion/datatables/integration/DomBaseIT.java
+++ datatables-jsp/src/test/java/com/github/dandelion/datatables/integration/DomBaseIT.java
@@ -57,8 +57,8 @@ import org.openqa.selenium.remote.service.DriverService;
 import com.github.dandelion.datatables.core.asset.JsResource;
 import com.github.dandelion.datatables.core.asset.WebResources;
 import com.github.dandelion.datatables.core.cache.AssetCache;
-import com.github.dandelion.datatables.utils.Mock;
-import com.github.dandelion.datatables.utils.Person;
+import com.github.dandelion.datatables.testing.core.Mock;
+import com.github.dandelion.datatables.testing.core.Person;
 
 /**
  * Base class for integration tests based on a DOM source.
