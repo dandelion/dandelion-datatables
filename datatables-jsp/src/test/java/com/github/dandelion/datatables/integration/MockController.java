ff76a63cffa01120de08666fb9c1b7f27e82054c
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-04-08 00:35:59 +0200

    New MockService for testing server-side processing

--- datatables-jsp/src/test/java/com/github/dandelion/datatables/integration/MockController.java
+++ datatables-jsp/src/test/java/com/github/dandelion/datatables/integration/MockController.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.dandelion.datatables.core.ajax.DataSet;
+import com.github.dandelion.datatables.core.ajax.DatatablesCriterias;
+import com.github.dandelion.datatables.core.ajax.DatatablesResponse;
 import com.github.dandelion.datatables.utils.Mock;
 import com.github.dandelion.datatables.utils.Person;
 
@@ -22,6 +26,9 @@ import com.github.dandelion.datatables.utils.Person;
 @RequestMapping(method = RequestMethod.GET)
 public class MockController {
 
+	@Autowired
+	private MockService mockService;
+	
 	@RequestMapping(value = "/basic/{page}")
 	public String goToBasicExample(@PathVariable String page) {
 		return "basic/" + page;
@@ -51,5 +58,13 @@ public class MockController {
 	public @ResponseBody
 	List<Person> findAll(HttpServletRequest request) {
 		return Mock.persons;
+	}
+	
+	@RequestMapping(value = "/persons1", method = RequestMethod.GET)
+	public @ResponseBody
+	DatatablesResponse<Person> findAllForDataTables(HttpServletRequest request) {
+		DatatablesCriterias criterias = DatatablesCriterias.getFromRequest(request);
+		DataSet<Person> persons = mockService.getData(criterias);
+		return DatatablesResponse.build(persons, criterias);
 	}
 }
