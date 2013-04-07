b26c634ad1a40feaa044c7f3753ccd935ebe32b9
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-04-08 00:22:22 +0200

    Formatting

--- datatables-thymeleaf/src/main/java/com/github/dandelion/datatables/thymeleaf/util/Utils.java
+++ datatables-thymeleaf/src/main/java/com/github/dandelion/datatables/thymeleaf/util/Utils.java
@@ -10,10 +10,11 @@ import com.github.dandelion.datatables.core.html.HtmlTable;
 
 public class Utils {
 
-	public static HtmlTable getTable(Arguments arguments){
-		return (HtmlTable)((IWebContext) arguments.getContext()).getHttpServletRequest().getAttribute("htmlTable");
+	public static HtmlTable getTable(Arguments arguments) {
+		return (HtmlTable) ((IWebContext) arguments.getContext()).getHttpServletRequest()
+				.getAttribute("htmlTable");
 	}
-	
+
 	/**
 	 * <p>
 	 * Return the base URL (context path included).
@@ -30,23 +31,32 @@ public class Utils {
 		return request.getRequestURL().toString()
 				.replace(request.getRequestURI(), request.getContextPath());
 	}
-	
+
 	/**
-	 * Use standard expression processor to selected value. 
-	 * If result is same class as default value, it's returned, otherwise default value is returned.
-	 * If value is null default value is returned. 
-	 * @param arguments {@link Arguments} instance for parsing
-	 * @param value String expression to parse
-	 * @param defaultValue <T> default value
-	 * @param clazz {@link Class} type
-	 * @return <T> result value or default value if result has different class type (or null)
+	 * Use standard expression processor to selected value. If result is same
+	 * class as default value, it's returned, otherwise default value is
+	 * returned. If value is null default value is returned.
+	 * 
+	 * @param arguments
+	 *            {@link Arguments} instance for parsing
+	 * @param value
+	 *            String expression to parse
+	 * @param defaultValue
+	 *            <T> default value
+	 * @param clazz
+	 *            {@link Class} type
+	 * @return <T> result value or default value if result has different class
+	 *         type (or null)
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T parseElementAttribute(Arguments arguments, String value, T defaultValue, Class<T> clazz) {
 		if (value == null) return defaultValue;
+		
 		Object result = StandardExpressionProcessor.processExpression(arguments, value.trim());
+		
 		if (result == null) return defaultValue;
 		T tmpValue = defaultValue;
+		
 		if (clazz.isAssignableFrom(result.getClass())) tmpValue = (T) result;
 		return tmpValue;
 	}
