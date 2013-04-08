2179eb9895f4d6f6c17943ad32a3183465be4fda
Gautier Dhordain <g.dhordain@gmail.com>
2013-04-08 22:49:47 +0200

    Thymeleaf expressions in displayLength attribute.
    Due to StandardExpressionProcessor.processExpression method, returned attrValue is a BigDecimal.
    Explicit conversion is required.

--- datatables-thymeleaf/src/main/java/com/github/dandelion/datatables/thymeleaf/processor/basic/TableDisplayLengthAttrProcessor.java
+++ datatables-thymeleaf/src/main/java/com/github/dandelion/datatables/thymeleaf/processor/basic/TableDisplayLengthAttrProcessor.java
@@ -1,11 +1,13 @@
 package com.github.dandelion.datatables.thymeleaf.processor.basic;
 
+import java.math.BigDecimal;
 import org.thymeleaf.Arguments;
 import org.thymeleaf.dom.Element;
 import org.thymeleaf.processor.IAttributeNameProcessorMatcher;
 import org.thymeleaf.processor.ProcessorResult;
 import com.github.dandelion.datatables.core.html.HtmlTable;
 import com.github.dandelion.datatables.thymeleaf.dialect.AbstractDatatablesAttrProcessor;
+import com.github.dandelion.datatables.thymeleaf.util.Utils;
 
 public class TableDisplayLengthAttrProcessor extends AbstractDatatablesAttrProcessor{
 
@@ -20,8 +22,8 @@ public class TableDisplayLengthAttrProcessor extends AbstractDatatablesAttrProce
 
 	@Override
 	protected ProcessorResult doProcessAttribute(Arguments arguments, Element element, String attributeName, HtmlTable table){
-		Integer attrValue = Integer.parseInt(element.getAttributeValue(attributeName));
-		table.setDisplayLength(attrValue);
+		BigDecimal attrValue = Utils.parseElementAttribute(arguments, element.getAttributeValue(attributeName), new BigDecimal(10), BigDecimal.class);
+		table.setDisplayLength(attrValue.intValueExact());
 		return ProcessorResult.ok();
 	}
 }
