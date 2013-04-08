55214cbc76001656af17de6099d4f125e70aa115
Gautier Dhordain <g.dhordain@gmail.com>
2013-04-08 23:47:53 +0200

    Utils.parseElementAttribute have now a specific behaviour for boolean
    Thymeleaf raises TemplateProcessingException when it tries to parse
    "false" or "true" as expression.

--- datatables-thymeleaf/src/main/java/com/github/dandelion/datatables/thymeleaf/util/Utils.java
+++ datatables-thymeleaf/src/main/java/com/github/dandelion/datatables/thymeleaf/util/Utils.java
@@ -1,14 +1,15 @@
 package com.github.dandelion.datatables.thymeleaf.util;
 
+import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
-
 import org.thymeleaf.Arguments;
 import org.thymeleaf.context.IWebContext;
 import org.thymeleaf.standard.expression.StandardExpressionProcessor;
-
 import com.github.dandelion.datatables.core.html.HtmlTable;
 
 public class Utils {
+
+	private static Pattern booleanRegex = Pattern.compile("^true|false$");;
 
 	public static HtmlTable getTable(Arguments arguments) {
 		return (HtmlTable) ((IWebContext) arguments.getContext()).getHttpServletRequest()
@@ -48,10 +49,39 @@ public class Utils {
 	 * @return <T> result value or default value if result has different class
 	 *         type (or null)
 	 */
-	@SuppressWarnings("unchecked")
 	public static <T> T parseElementAttribute(Arguments arguments, String value, T defaultValue, Class<T> clazz) {
 		if (value == null) return defaultValue;
 		
+		return processStandardExpression(arguments, value, defaultValue, clazz);
+	}
+
+
+	/**
+	 * Use standard expression processor to selected value. If result is same
+	 * class as default value, it's returned, otherwise default value is
+	 * returned. If value is null default value is returned.
+	 * 
+	 * @param arguments
+	 *            {@link Arguments} instance for parsing
+	 * @param value
+	 *            String expression to parse
+	 * @param defaultValue
+	 *            Boolean default value
+	 * @param clazz
+	 *            {@link Class} type
+	 * @return Boolean result value or default value if result has different class
+	 *         type (or null)
+	 */
+	public static Boolean parseElementAttribute(Arguments arguments, String value, Boolean defaultValue, Class<Boolean> clazz) {
+		if (value == null) return defaultValue;
+		
+		if(booleanRegex.matcher(value).find()) return Boolean.parseBoolean(value);
+		
+		return processStandardExpression(arguments, value, defaultValue, clazz);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T> T processStandardExpression(Arguments arguments, String value, T defaultValue, Class<T> clazz){
 		Object result = StandardExpressionProcessor.processExpression(arguments, value.trim());
 		
 		if (result == null) return defaultValue;
