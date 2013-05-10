c37f84abd7e44daf7f09fb1546991befb0c67577
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-05-10 10:19:06 +0200

    New abstract views that will be used in export

---
+++ datatables-core/src/main/java/com/github/dandelion/datatables/core/view/AbstractDatatablesView.java
@@ -0,0 +1,115 @@
+/*
+ * [The "BSD licence"]
+ * Copyright (c) 2012 Dandelion
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of Dandelion nor the names of its contributors 
+ * may be used to endorse or promote products derived from this software 
+ * without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.github.dandelion.datatables.core.view;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <p>
+ * Abstract base class for all view implementations used in export.
+ * <p>
+ * Provides support for static attributes and utilities, to be made available to
+ * the view.
+ * 
+ * <p>
+ * Highly inspired by the AbstractView Spring class, written by Rod Johnson and
+ * Juergen Hoeller.
+ * 
+ * @author Thibault Duchateau
+ */
+public abstract class AbstractDatatablesView {
+
+	/** Default content type. Overridable as bean property. */
+	public static final String DEFAULT_CONTENT_TYPE = "text/html;charset=ISO-8859-1";
+
+	private String contentType = DEFAULT_CONTENT_TYPE;
+
+	/**
+	 * Set the content type for this view. Default is
+	 * "text/html;charset=ISO-8859-1".
+	 * <p>
+	 * May be ignored by subclasses if the view itself is assumed to set the
+	 * content type, e.g. in case of JSPs.
+	 */
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	/**
+	 * Return the content type for this view.
+	 */
+	public String getContentType() {
+		return this.contentType;
+	}
+
+	/**
+	 * Write the given temporary OutputStream to the HTTP response.
+	 * 
+	 * @param response
+	 *            current HTTP response
+	 * @param baos
+	 *            the temporary OutputStream to write
+	 * @throws IOException
+	 *             if writing/flushing failed
+	 */
+	protected void writeToResponse(HttpServletResponse response, ByteArrayOutputStream baos) throws IOException {
+		// Write content type and also length (determined via byte array).
+		response.setContentType(getContentType());
+		response.setContentLength(baos.size());
+
+		// Flush byte array to servlet output stream.
+		ServletOutputStream out = response.getOutputStream();
+		baos.writeTo(out);
+		out.flush();
+	}
+
+	/**
+	 * Write the given temporary OutputStream to the HTTP response as an
+	 * Attachment with the given title.
+	 * 
+	 * @param response
+	 *            current HTTP response
+	 * @param baos
+	 *            the temporary OutputStream to write
+	 * @param title
+	 *            the title of the attachment
+	 * @throws IOException
+	 *             if writing/flushing failed
+	 */
+	protected void writeToResponse(HttpServletResponse response, ByteArrayOutputStream baos, String title)
+			throws IOException {
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + title + "\"");
+		writeToResponse(response, baos);
+	}
+}
