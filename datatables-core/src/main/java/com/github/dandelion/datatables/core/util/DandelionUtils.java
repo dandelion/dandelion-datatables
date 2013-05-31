c5ba23ec15a9833b2f27b51eb0fb6b750506bce0
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-05-31 13:30:07 +0200

    SystemConstants is now used everywhere

--- datatables-core/src/main/java/com/github/dandelion/datatables/core/util/DandelionUtils.java
+++ datatables-core/src/main/java/com/github/dandelion/datatables/core/util/DandelionUtils.java
@@ -2,15 +2,18 @@ package com.github.dandelion.datatables.core.util;
 
 import org.apache.commons.lang.StringUtils;
 
+import com.github.dandelion.datatables.core.constants.SystemConstants;
+
 /**
  * Main utility methods.
- *
+ * 
  * @author Thibault Duchateau
  * @since 0.8.6
  */
 public class DandelionUtils {
 
-	public static boolean isDevModeEnabled(){
-		return StringUtils.isBlank(System.getProperty("dandelion.dev.mode")) || "true".equals(System.getProperty("dandelion.dev.mode")); 
+	public static boolean isDevModeEnabled() {
+		return StringUtils.isBlank(System.getProperty(SystemConstants.DANDELION_DEV_MODE))
+				|| "true".equals(System.getProperty(SystemConstants.DANDELION_DEV_MODE));
 	}
 }
