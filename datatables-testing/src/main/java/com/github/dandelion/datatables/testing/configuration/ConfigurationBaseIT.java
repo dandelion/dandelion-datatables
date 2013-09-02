de950c3a96b1b02b51fc26d0543d72f02c2098e4
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-09-02 22:22:54 +0200

    Removed logging from IT

--- datatables-testing/src/main/java/com/github/dandelion/datatables/testing/configuration/ConfigurationBaseIT.java
+++ datatables-testing/src/main/java/com/github/dandelion/datatables/testing/configuration/ConfigurationBaseIT.java
@@ -11,14 +11,12 @@ public class ConfigurationBaseIT extends BaseIT {
 	@Test
 	public void should_use_overriden_global_configuration() throws Exception {
 		goToPage("configuration/global_overriden");
-		System.out.println(driver.getPageSource());
 		assertThat(getTable().getAttribute("class")).contains("myClass");
 	}
 	
 	@Test
 	public void should_use_group1_configuration() throws Exception {
 		goToPage("configuration/enable_custom_group");
-		System.out.println(driver.getPageSource());
 		assertThat(getTable().getAttribute("class")).contains("my-group1-class");
 	}
 }
