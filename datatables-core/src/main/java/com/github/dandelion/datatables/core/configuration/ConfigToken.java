dandelion/dandelion-datatables

commit dc0bf449d9f4cfa7763eeea21f8aa5f11036e7a7

[Core][JSP] Fixed a regression with the configured message resolver
* Removed the unused I18N_LOCALE_RESOLVER configuration from TableConfig. The locale resolver is particular since it is initialized in the ConfigurationLoader
 * Removed the old setInternalMessageResolver method. The configured message resolver is now part of the configurations map.
 * Removed code duplication in constructors of HtmlTable and forced a first pass of configuration processing in order to make available the actual implementation of MessageResolver in the JSP ColumnTag
 * Fixed the MessageResolverProcessor by updating the configuration map

Closes #253

datatables-core/src/main/java/com/github/dandelion/datatables/core/configuration/TableConfig.java
@@ -41,7 +41,6 @@ import com.github.dandelion.datatables.core.extension.feature.FilterPlaceholder;
 import com.github.dandelion.datatables.core.extension.feature.PaginationType;
 import com.github.dandelion.datatables.core.extension.theme.ThemeOption;
 import com.github.dandelion.datatables.core.html.HtmlTable;
-import com.github.dandelion.datatables.core.i18n.LocaleResolver;
 import com.github.dandelion.datatables.core.i18n.MessageResolver;
 import com.github.dandelion.datatables.core.processor.BooleanProcessor;
 import com.github.dandelion.datatables.core.processor.ConfigurationProcessor;
@@ -210,7 +209,6 @@ public final class TableConfig {
 	public static ConfigToken<String> EXPORT_MIMETYPE = new ConfigToken<String>(ExportFormatProcessor.REGEX_EXPORT_MIMETYPE, new ExportFormatProcessor());
 	
 	public static ConfigToken<MessageResolver> I18N_MESSAGE_RESOLVER = new ConfigToken<MessageResolver>(P_I18N_MESSAGE_RESOLVER, new MessageResolverProcessor());
-	public static ConfigToken<LocaleResolver> I18N_LOCALE_RESOLVER = new ConfigToken<LocaleResolver>(P_I18N_LOCALE_RESOLVER, new MessageResolverProcessor());
 	public static ConfigToken<String> I18N_MSG_PROCESSING = new ConfigToken<String>(P_I18N_MSG_PROCESSING, new MessageProcessor());
 	public static ConfigToken<String> I18N_MSG_SEARCH = new ConfigToken<String>(P_I18N_MSG_SEARCH, new MessageProcessor());
 	public static ConfigToken<String> I18N_MSG_LENGTHMENU = new ConfigToken<String>(P_I18N_MSG_LENGTHMENU, new MessageProcessor());
@@ -289,7 +287,6 @@ public final class TableConfig {
 		internalConf.put(formalize(ExportFormatProcessor.REGEX_EXPORT_MIMETYPE), EXPORT_MIMETYPE);
 		
 		internalConf.put(formalize(P_I18N_MESSAGE_RESOLVER), I18N_MESSAGE_RESOLVER);
-		internalConf.put(formalize(P_I18N_LOCALE_RESOLVER), I18N_LOCALE_RESOLVER);
 		internalConf.put(formalize(P_I18N_MSG_PROCESSING), I18N_MSG_PROCESSING);
 		internalConf.put(formalize(P_I18N_MSG_SEARCH), I18N_MSG_SEARCH);
 		internalConf.put(formalize(P_I18N_MSG_LENGTHMENU), I18N_MSG_LENGTHMENU);

commit 5ff89bed0a3bcc66ecf8dd48778f7987a6ee630a

[Core] Fixed issue with configuration processing introduced in dc0bf449d9f4cfa7763eeea21f8aa5f11036e7a7 The staging configuration was processed too early, introducing several bugs at runtime Closes #257 Closes #258

datatables-core/src/main/java/com/github/dandelion/datatables/core/configuration/ConfigToken.java
@@ -77,7 +77,8 @@ public class ConfigToken<T> {
 	public T valueFrom(TableConfiguration tableConfiguration) {
 		if (tableConfiguration.getConfigurations() != null) {
 			return (T) tableConfiguration.getConfigurations().get(this);
-		} else {
+		}
+		else {
 			return null;
 		}
 	}
@@ -91,7 +92,8 @@ public class ConfigToken<T> {
 	public T valueFrom(HtmlTable table) {
 		if (table.getTableConfiguration().getConfigurations() != null) {
 			return (T) table.getTableConfiguration().getConfigurations().get(this);
-		} else {
+		}
+		else {
 			return null;
 		}
 	}
@@ -115,18 +117,19 @@ public class ConfigToken<T> {
 		}
 	}
 
-	public void toto(){
-		
+	public void toto() {
+
 	}
-	
+
 	public void appendIn(TableConfiguration tableConfiguration, char value) {
-		tableConfiguration.getConfigurations().put(this, ((StringBuilder) tableConfiguration.getConfigurations().get(this)).append(value));
+		tableConfiguration.getConfigurations().put(this,
+				((StringBuilder) tableConfiguration.getConfigurations().get(this)).append(value));
 	}
 
 	public void appendIn(ColumnConfiguration columnConfiguration, String value) {
 		doAppendIn(columnConfiguration, value);
 	}
-	
+
 	public void appendIn(ColumnConfiguration columnConfiguration, char value) {
 		doAppendIn(columnConfiguration, String.valueOf(value));
 	}
@@ -142,7 +145,7 @@ public class ConfigToken<T> {
 			}
 		}
 	}
-	
+
 	public void setIn(T value, HtmlTable table) {
 		table.getTableConfiguration().getConfigurations().put(this, value);
 	}
@@ -151,6 +154,5 @@ public class ConfigToken<T> {
 	public String toString() {
 		return "['" + propertyName + "' / " + processor.getClass().getSimpleName() + "]";
 	}
-	
-	
+
 }

datatables-core/src/main/java/com/github/dandelion/datatables/core/configuration/ConfigurationLoader.java
@@ -55,7 +55,9 @@ public interface ConfigurationLoader {
 	public final static String DT_USER_PROPERTIES = "datatables";
 	public final static String DEFAULT_GROUP_NAME = "global";
 	public final static String STANDALONE_BUNDLES_TO_EXCLUDE = "bootstrap-datepicker,bootstrap2,bootstrap3,jquery,jqueryui,moment";
-
+	public final static String I18N_LOCALE_RESOLVER = "i18n.locale.resolver";
+	public final static String I18N_MESSAGE_RESOLVER = "i18n.message.resolver";
+	
 	/**
 	 * <p>
 	 * Load the default configuration from the internal properties file and
@@ -68,7 +70,7 @@ public interface ConfigurationLoader {
 	 * @return the default properties
 	 */
 	public Properties loadDefaultConfiguration();
-
+	
 	/**
 	 * <p>
 	 * Load the user configuration which can be localized thanks to the given

datatables-core/src/main/java/com/github/dandelion/datatables/core/configuration/ConfigurationStore.java
@@ -42,23 +42,23 @@ import com.github.dandelion.core.Context;
 import com.github.dandelion.core.utils.StringUtils;
 import com.github.dandelion.core.web.WebConstants;
 import com.github.dandelion.datatables.core.exception.UnkownGroupException;
+import com.github.dandelion.datatables.core.i18n.MessageResolver;
 
 /**
  * Storage class for all configurations by Locale and group.
- *
+ * 
  * @author Thibault Duchateau
  * @since 0.9.0
  */
 public class ConfigurationStore {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(ConfigurationStore.class);
-	
+
 	/**
 	 * Static map containing all configurations
 	 */
-	private static Map<Locale, Map<String, Map<ConfigToken<?>, Object>>> configurationStore = 
-			new HashMap<Locale, Map<String, Map<ConfigToken<?>, Object>>>();
-	
+	private static Map<Locale, Map<String, Map<ConfigToken<?>, Object>>> configurationStore = new HashMap<Locale, Map<String, Map<ConfigToken<?>, Object>>>();
+
 	/**
 	 * <p>
 	 * Return the {@link TableConfiguration} prototype for the given locale
@@ -72,17 +72,19 @@ public class ConfigurationStore {
 	 * @return the stored proptotype of a {@link TableConfiguration}.
 	 */
 	public static TableConfiguration getPrototype(HttpServletRequest request, String groupName) {
-		
+
 		Locale locale = null;
 		String group = StringUtils.isBlank(groupName) ? ConfigurationLoader.DEFAULT_GROUP_NAME : groupName;
-		
-		// Retrieve the locale either from a configured LocaleResolver or using the default locale
+
+		// Retrieve the locale either from a configured LocaleResolver or using
+		// the default locale
 		if (request != null) {
 			locale = DatatablesConfigurator.getLocaleResolver().resolveLocale(request);
-		} else {
+		}
+		else {
 			locale = Locale.getDefault();
 		}
-        
+
 		Context context = (Context) request.getAttribute(WebConstants.DANDELION_CONTEXT_ATTRIBUTE);
 		if (context == null) {
 			LOGGER.warn("The Dandelion context doesn't seem to be available. Did you forget to declare the DandelionFilter in your web.xml file?");
@@ -90,11 +92,11 @@ public class ConfigurationStore {
 		else if (context.isDevModeEnabled()) {
 			clear();
 		}
-		
-		if(!configurationStore.containsKey(locale)){
+
+		if (!configurationStore.containsKey(locale)) {
 			resolveGroupsForLocale(locale, request);
 		}
-			
+
 		if (!configurationStore.get(locale).containsKey(group)) {
 			StringBuilder msg = new StringBuilder("The group '");
 			msg.append(group);
@@ -102,8 +104,10 @@ public class ConfigurationStore {
 			msg.append(configurationStore.get(locale).keySet());
 			throw new UnkownGroupException(msg.toString());
 		}
-		
-		return new TableConfiguration(configurationStore.get(locale).get(group), request);
+
+		MessageResolver messageResolver = DatatablesConfigurator.getMessageResolver(request);
+
+		return new TableConfiguration(configurationStore.get(locale).get(group), messageResolver, request);
 	}
 
 	/**
@@ -114,30 +118,30 @@ public class ConfigurationStore {
 	 */
 	public static void resolveGroupsForLocale(Locale locale, HttpServletRequest request) {
 		Map<String, Map<ConfigToken<?>, Object>> map = new HashMap<String, Map<ConfigToken<?>, Object>>();
-		
+
 		ConfigurationLoader confLoader = DatatablesConfigurator.getConfigurationLoader();
 
 		confLoader.loadDefaultConfiguration();
 		confLoader.loadUserConfiguration(locale);
-		confLoader.resolveGroups(locale);		
+		confLoader.resolveGroups(locale);
 		confLoader.resolveConfigurations(map, locale, request);
-		
+
 		configurationStore.put(locale, map);
 	}
-	
+
 	/**
 	 * <b>FOR INTERNAL USE ONLY</b>
 	 * 
 	 * @return
 	 */
-	public static Map<Locale, Map<String, Map<ConfigToken<?>, Object>>> getConfigurationStore(){
+	public static Map<Locale, Map<String, Map<ConfigToken<?>, Object>>> getConfigurationStore() {
 		return configurationStore;
 	}
-	
+
 	/**
 	 * <b>FOR INTERNAL USE ONLY</b>
 	 */
-	public static void clear(){
+	public static void clear() {
 		configurationStore.clear();
 	}
 }

datatables-core/src/main/java/com/github/dandelion/datatables/core/configuration/DatatablesConfigurator.java
@@ -33,6 +33,8 @@ import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.Properties;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,6 +45,7 @@ import com.github.dandelion.datatables.core.exception.ConfigurationLoadingExcept
 import com.github.dandelion.datatables.core.generator.javascript.JavascriptGenerator;
 import com.github.dandelion.datatables.core.generator.javascript.StandardJavascriptGenerator;
 import com.github.dandelion.datatables.core.i18n.LocaleResolver;
+import com.github.dandelion.datatables.core.i18n.MessageResolver;
 import com.github.dandelion.datatables.core.i18n.StandardLocaleResolver;
 
 /**
@@ -72,6 +75,7 @@ public class DatatablesConfigurator {
 
 	private static ConfigurationLoader configurationLoader;
 	private static LocaleResolver localeResolver;
+	private static MessageResolver messageResolver;
 
 	/**
 	 * Return a uniq implementation of {@link LocaleResolver} using the
@@ -129,6 +133,49 @@ public class DatatablesConfigurator {
 		return localeResolver;
 	}
 
+	@SuppressWarnings("unchecked")
+	public static MessageResolver getMessageResolver(HttpServletRequest request) {
+		Properties userProperties = null;
+		String className = null;
+		ConfigurationLoader configurationLoader = getConfigurationLoader();
+
+		if (messageResolver == null) {
+
+			try {
+				userProperties = configurationLoader.loadUserConfiguration(Locale.getDefault());
+
+				if(userProperties != null){
+					try {
+							className = userProperties.getProperty("i18n.message.resolver");
+					} catch (MissingResourceException e) {
+	
+						logger.debug("No custom MessageResolver has been configured. Using default one.");
+					}
+				}
+
+				if(className == null){
+					Properties defaultProperties = configurationLoader.loadDefaultConfiguration();
+					className = defaultProperties.getProperty("i18n.message.resolver");
+				}
+
+				if (className != null) {
+						Class<MessageResolver> classProperty;
+						try {
+							classProperty = (Class<MessageResolver>) ClassUtils.getClass(className);
+							messageResolver = classProperty.getDeclaredConstructor(new Class[] { HttpServletRequest.class })
+									.newInstance(request);
+						} catch (Exception e) {
+							throw new ConfigurationLoadingException(e);
+						}
+				}
+			} catch (ConfigurationLoadingException e) {
+				throw new ConfigurationLoadingException("Unable to retrieve the MessageResolver using the class '"
+						+ className + "'", e);
+			}
+		}
+		return messageResolver;
+	}
+	
 	/**
 	 * <p>
 	 * Returns an implementation of {@link ConfigurationLoader} using the

datatables-core/src/main/java/com/github/dandelion/datatables/core/configuration/StandardConfigurationLoader.java
@@ -188,7 +188,7 @@ public class StandardConfigurationLoader implements ConfigurationLoader {
 		if (userProperties != null && !userProperties.isEmpty()) {
 			for (Entry<Object, Object> entry : userProperties.entrySet()) {
 				String key = entry.getKey().toString();
-				if (!key.contains("i18n.locale.resolver") && !key.contains("main.standalone")) {
+				if (!key.contains(I18N_LOCALE_RESOLVER) && !key.contains(I18N_MESSAGE_RESOLVER) && !key.contains("main.standalone")) {
 					groups.add(key.substring(0, key.indexOf(".")));
 				}
 			}
@@ -222,7 +222,7 @@ public class StandardConfigurationLoader implements ConfigurationLoader {
 		Properties globalProperties = new Properties();
 		for (Entry<Object, Object> entry : defaultProperties.entrySet()) {
 			String key = entry.getKey().toString();
-			if (!key.equals("i18n.locale.resolver") && !key.equals("main.standalone")) {
+			if (!key.equals(I18N_LOCALE_RESOLVER) && !key.equals(I18N_MESSAGE_RESOLVER) && !key.equals("main.standalone")) {
 				globalProperties.put(key.substring(key.indexOf(".") + 1), entry.getValue());
 			}
 		}
@@ -266,7 +266,7 @@ public class StandardConfigurationLoader implements ConfigurationLoader {
 				if (configToken != null) {
 					userConf.put(configToken, entry.getValue().toString());
 				}
-				else {
+				else if(!key.equals(I18N_LOCALE_RESOLVER) && !key.equals(I18N_MESSAGE_RESOLVER)) {
 					if (wrongKeys.containsKey(groupName)) {
 						wrongKeys.get(groupName).add(key);
 					}
@@ -319,14 +319,14 @@ public class StandardConfigurationLoader implements ConfigurationLoader {
 			if (!userProps.isEmpty()) {
 				for (Entry<Object, Object> entry : userProps.entrySet()) {
 					String key = entry.getKey().toString();
-					if (key.contains(TableConfig.I18N_MESSAGE_RESOLVER.getPropertyName())
+					if (key.contains(I18N_MESSAGE_RESOLVER)
 							&& StringUtils.isBlank(entry.getValue().toString())) {
 						userProps.put(entry.getKey(), "com.github.dandelion.datatables.jsp.i18n.JstlMessageResolver");
 					}
 				}
 			}
 			else {
-				userProps.put("global." + TableConfig.I18N_MESSAGE_RESOLVER.getPropertyName(),
+				userProps.put("global." + I18N_MESSAGE_RESOLVER,
 						"com.github.dandelion.datatables.jsp.i18n.JstlMessageResolver");
 			}
 		}

datatables-core/src/main/java/com/github/dandelion/datatables/core/configuration/TableConfig.java
@@ -41,7 +41,6 @@ import com.github.dandelion.datatables.core.extension.feature.FilterPlaceholder;
 import com.github.dandelion.datatables.core.extension.feature.PaginationType;
 import com.github.dandelion.datatables.core.extension.theme.ThemeOption;
 import com.github.dandelion.datatables.core.html.HtmlTable;
-import com.github.dandelion.datatables.core.i18n.MessageResolver;
 import com.github.dandelion.datatables.core.processor.BooleanProcessor;
 import com.github.dandelion.datatables.core.processor.ConfigurationProcessor;
 import com.github.dandelion.datatables.core.processor.IntegerProcessor;
@@ -63,7 +62,6 @@ import com.github.dandelion.datatables.core.processor.feature.FeatureFilterSelec
 import com.github.dandelion.datatables.core.processor.feature.FeatureLengthMenuProcessor;
 import com.github.dandelion.datatables.core.processor.feature.FeaturePaginationTypeProcessor;
 import com.github.dandelion.datatables.core.processor.i18n.MessageProcessor;
-import com.github.dandelion.datatables.core.processor.i18n.MessageResolverProcessor;
 import com.github.dandelion.datatables.core.processor.main.MainExtensionNamesProcessor;
 
 /**
@@ -132,8 +130,6 @@ public final class TableConfig {
 	public static final String P_EXPORT_CONTAINER_STYLE = "export.container.style";
 	public static final String P_EXPORT_CONTAINER_CLASS = "export.container.class";
 	
-	public static final String P_I18N_MESSAGE_RESOLVER = "i18n.message.resolver";
-	public static final String P_I18N_LOCALE_RESOLVER = "i18n.locale.resolver";
 	public static final String P_I18N_MSG_PROCESSING = "i18n.msg.processing";
 	public static final String P_I18N_MSG_SEARCH = "i18n.msg.search";
 	public static final String P_I18N_MSG_LENGTHMENU = "i18n.msg.lengthmenu";
@@ -208,7 +204,6 @@ public final class TableConfig {
 	public static ConfigToken<String> EXPORT_FILENAME = new ConfigToken<String>(ExportFormatProcessor.REGEX_EXPORT_FILENAME, new ExportFormatProcessor());
 	public static ConfigToken<String> EXPORT_MIMETYPE = new ConfigToken<String>(ExportFormatProcessor.REGEX_EXPORT_MIMETYPE, new ExportFormatProcessor());
 	
-	public static ConfigToken<MessageResolver> I18N_MESSAGE_RESOLVER = new ConfigToken<MessageResolver>(P_I18N_MESSAGE_RESOLVER, new MessageResolverProcessor());
 	public static ConfigToken<String> I18N_MSG_PROCESSING = new ConfigToken<String>(P_I18N_MSG_PROCESSING, new MessageProcessor());
 	public static ConfigToken<String> I18N_MSG_SEARCH = new ConfigToken<String>(P_I18N_MSG_SEARCH, new MessageProcessor());
 	public static ConfigToken<String> I18N_MSG_LENGTHMENU = new ConfigToken<String>(P_I18N_MSG_LENGTHMENU, new MessageProcessor());
@@ -286,7 +281,6 @@ public final class TableConfig {
 		internalConf.put(formalize(ExportFormatProcessor.REGEX_EXPORT_FILENAME), EXPORT_FILENAME);
 		internalConf.put(formalize(ExportFormatProcessor.REGEX_EXPORT_MIMETYPE), EXPORT_MIMETYPE);
 		
-		internalConf.put(formalize(P_I18N_MESSAGE_RESOLVER), I18N_MESSAGE_RESOLVER);
 		internalConf.put(formalize(P_I18N_MSG_PROCESSING), I18N_MSG_PROCESSING);
 		internalConf.put(formalize(P_I18N_MSG_SEARCH), I18N_MSG_SEARCH);
 		internalConf.put(formalize(P_I18N_MSG_LENGTHMENU), I18N_MSG_LENGTHMENU);

datatables-core/src/main/java/com/github/dandelion/datatables/core/configuration/TableConfiguration.java
@@ -48,6 +48,7 @@ import com.github.dandelion.datatables.core.export.ExportConf;
 import com.github.dandelion.datatables.core.extension.Extension;
 import com.github.dandelion.datatables.core.html.ExtraHtml;
 import com.github.dandelion.datatables.core.html.HtmlTag;
+import com.github.dandelion.datatables.core.i18n.MessageResolver;
 
 /**
  * Contains all the table configuration.
@@ -73,6 +74,7 @@ public class TableConfiguration {
 
 	// I18n
 	private Properties messages = new Properties();
+	private MessageResolver messageResolver;
 
 	private Set<Extension> internalExtensions;
 	private String tableId;
@@ -123,8 +125,9 @@ public class TableConfiguration {
 	 * 
 	 * @param userConf
 	 */
-	public TableConfiguration(Map<ConfigToken<?>, Object> userConf, HttpServletRequest request) {
+	public TableConfiguration(Map<ConfigToken<?>, Object> userConf, MessageResolver messageResolver, HttpServletRequest request) {
 		this.request = request;
+		this.messageResolver = messageResolver;
 		this.configurations = userConf;
 		this.stagingConfiguration = new HashMap<ConfigToken<?>, Object>();
 		this.exportConfiguration = new LinkedHashMap<String, ExportConf>();
@@ -142,6 +145,7 @@ public class TableConfiguration {
 	private TableConfiguration(String tableId, TableConfiguration objectToClone, HttpServletRequest request) {
 		this.request = request;
 		this.tableId = tableId;
+		this.messageResolver = objectToClone.messageResolver;
 		this.configurations = objectToClone.configurations;
 		this.stagingConfiguration = objectToClone.stagingConfiguration;
 		this.exportConfiguration = objectToClone.exportConfiguration;
@@ -171,7 +175,7 @@ public class TableConfiguration {
 	}
 
 	public void set(ConfigToken<?> configToken, Object object) {
-		configurations.put(configToken, object);
+		this.configurations.put(configToken, object);
 	}
 
 	public Map<ConfigToken<?>, Object> getConfigurations() {
@@ -321,6 +325,14 @@ public class TableConfiguration {
 		return messages;
 	}
 
+	public MessageResolver getMessageResolver() {
+		return messageResolver;
+	}
+
+	public void setMessageResolver(MessageResolver messageResolver) {
+		this.messageResolver = messageResolver;
+	}
+
 	public void setMessages(Properties messages) {
 		this.messages = messages;
 	}
@@ -341,7 +353,7 @@ public class TableConfiguration {
 		if (extraHtmls == null) {
 			extraHtmls = new ArrayList<ExtraHtml>();
 		}
-		extraHtmls.add(extraHtml);
+		this.extraHtmls.add(extraHtml);
 	}
 
 	public String getCurrentExportFormat() {

datatables-core/src/main/java/com/github/dandelion/datatables/core/html/HtmlTable.java
@@ -68,7 +68,6 @@ public class HtmlTable extends HtmlTag {
 		this.id = processId(id);
 		this.dynamicAttributes = dynamicAttributes;
 		this.tableConfiguration = TableConfiguration.getInstance(id, request, groupName);
-		TableConfig.processConfiguration(this);
 	}
 
 	/**

datatables-core/src/main/java/com/github/dandelion/datatables/core/processor/i18n/MessageResolverProcessor.java
@@ -1,80 +1,0 @@
-/*
- * [The "BSD licence"]
- * Copyright (c) 2012 Dandelion
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 
- * 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * 3. Neither the name of Dandelion nor the names of its contributors 
- * may be used to endorse or promote products derived from this software 
- * without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-package com.github.dandelion.datatables.core.processor.i18n;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.github.dandelion.datatables.core.configuration.TableConfig;
-import com.github.dandelion.datatables.core.i18n.MessageResolver;
-import com.github.dandelion.datatables.core.processor.AbstractConfigurationProcessor;
-import com.github.dandelion.datatables.core.util.ClassUtils;
-
-/**
- * <p>
- * Processor in charge of instanciating the configured message resolver.
- * 
- * @author Thibault Duchateau
- * @since 0.9.0
- * @see TableConfig#I18N_MESSAGE_RESOLVER
- */
-public class MessageResolverProcessor extends AbstractConfigurationProcessor {
-
-	// Logger
-	private static Logger logger = LoggerFactory.getLogger(MessageResolverProcessor.class);
-
-	@Override
-	@SuppressWarnings("unchecked")
-	public void doProcess() {
-
-		MessageResolver messageResolver = null;
-
-		if (stringifiedValue != null) {
-			try {
-				Class<MessageResolver> classProperty = (Class<MessageResolver>) ClassUtils.getClass(stringifiedValue);
-				messageResolver = classProperty.getDeclaredConstructor(new Class[] { HttpServletRequest.class })
-						.newInstance(tableConfiguration.getRequest());
-
-				logger.info("MessageResolver initialized with {}", messageResolver.getClass().getSimpleName());
-			}
-			catch (Exception e) {
-				logger.warn("Unable to instantiate the configured {} due to a {} exception", stringifiedValue, e
-						.getClass().getName(), e);
-			}
-		}
-		else {
-			logger.info("No {} configured", MessageResolver.class.getSimpleName());
-		}
-
-		updateEntry(messageResolver);
-	}
-}

datatables-extras/datatables-spring3/src/main/java/com/github/dandelion/datatables/extras/spring3/i18n/SpringMessageResolver.java
@@ -84,7 +84,7 @@ public class SpringMessageResolver extends AbstractMessageResolver {
 			try {
 				message = messageSource.getMessage(messageKey, null, locale);
 			} catch (NoSuchMessageException e) {
-				logger.warn("No message found with the key The message key {} and locale {}.", messageKey, locale);
+				logger.warn("No message found with the key {} and locale {}.", messageKey, locale);
 				if (StringUtils.isBlank(message)) {
 					message = UNDEFINED_KEY + messageKey + UNDEFINED_KEY;
 				}

datatables-core/src/test/java/com/github/dandelion/datatables/core/processor/BooleanProcessorTest.java
@@ -31,7 +31,7 @@ public class BooleanProcessorTest {
 		MockPageContext mockPageContext = new MockPageContext(mockServletContext);
 		request = (HttpServletRequest) mockPageContext.getRequest();
 		confToBeApplied = new HashMap<ConfigToken<?>, Object>();
-		tableConfiguration = new TableConfiguration(confToBeApplied, request);
+		tableConfiguration = new TableConfiguration(confToBeApplied, null, request);
 		columnConfiguration = new ColumnConfiguration();
 	}
 	

datatables-core/src/test/java/com/github/dandelion/datatables/core/processor/ColumnProcessorBaseTest.java
@@ -61,7 +61,7 @@ public abstract class ColumnProcessorBaseTest {
 		MockPageContext mockPageContext = new MockPageContext(mockServletContext);
 		request = (HttpServletRequest) mockPageContext.getRequest();
 		confToBeApplied = new HashMap<ConfigToken<?>, Object>();
-		tableConfiguration = new TableConfiguration(confToBeApplied, request);
+		tableConfiguration = new TableConfiguration(confToBeApplied, null, request);
 		tableConfiguration.setTableId("fakeId");
 		columnConfiguration = new ColumnConfiguration();
 	}

datatables-core/src/test/java/com/github/dandelion/datatables/core/processor/EmptyStringProcessorTest.java
@@ -31,7 +31,7 @@ public class EmptyStringProcessorTest {
 		MockPageContext mockPageContext = new MockPageContext(mockServletContext);
 		request = (HttpServletRequest) mockPageContext.getRequest();
 		confToBeApplied = new HashMap<ConfigToken<?>, Object>();
-		tableConfiguration = new TableConfiguration(confToBeApplied, request);
+		tableConfiguration = new TableConfiguration(confToBeApplied, null, request);
 		columnConfiguration = new ColumnConfiguration();
 	}
 

datatables-core/src/test/java/com/github/dandelion/datatables/core/processor/IntegerProcessorTest.java
@@ -32,7 +32,7 @@ public class IntegerProcessorTest {
 		MockPageContext mockPageContext = new MockPageContext(mockServletContext);
 		request = (HttpServletRequest) mockPageContext.getRequest();
 		confToBeApplied = new HashMap<ConfigToken<?>, Object>();
-		tableConfiguration = new TableConfiguration(confToBeApplied, request);
+		tableConfiguration = new TableConfiguration(confToBeApplied, null, request);
 		columnConfiguration = new ColumnConfiguration();
 	}
 	

datatables-core/src/test/java/com/github/dandelion/datatables/core/processor/StringBuilderProcessorTest.java
@@ -31,7 +31,7 @@ public class StringBuilderProcessorTest {
 		MockPageContext mockPageContext = new MockPageContext(mockServletContext);
 		request = (HttpServletRequest) mockPageContext.getRequest();
 		confToBeApplied = new HashMap<ConfigToken<?>, Object>();
-		tableConfiguration = new TableConfiguration(confToBeApplied, request);
+		tableConfiguration = new TableConfiguration(confToBeApplied, null, request);
 		columnConfiguration = new ColumnConfiguration();
 	}
 

datatables-core/src/test/java/com/github/dandelion/datatables/core/processor/StringProcessorTest.java
@@ -37,7 +37,7 @@ public class StringProcessorTest {
 		request = (HttpServletRequest) mockPageContext.getRequest();
 		request.setAttribute(WebConstants.DANDELION_CONTEXT_ATTRIBUTE, new Context(new MockFilterConfig()));
 		confToBeApplied = new HashMap<ConfigToken<?>, Object>();
-		tableConfiguration = new TableConfiguration(confToBeApplied, request);
+		tableConfiguration = new TableConfiguration(confToBeApplied, null, request);
 		columnConfiguration = new ColumnConfiguration();
 	}
 

datatables-core/src/test/java/com/github/dandelion/datatables/core/processor/TableProcessorBaseTest.java
@@ -63,7 +63,7 @@ public abstract class TableProcessorBaseTest {
 		request = (HttpServletRequest) mockPageContext.getRequest();
 		request.setAttribute(WebConstants.DANDELION_CONTEXT_ATTRIBUTE, new Context(new MockFilterConfig()));
 		confToBeApplied = new HashMap<ConfigToken<?>, Object>();
-		tableConfiguration = new TableConfiguration(confToBeApplied, request);
+		tableConfiguration = new TableConfiguration(confToBeApplied, null, request);
 		tableConfiguration.setTableId("fakeId");
 	}
 	