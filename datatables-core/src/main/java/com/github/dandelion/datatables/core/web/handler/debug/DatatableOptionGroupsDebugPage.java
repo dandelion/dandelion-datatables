d75e9e6815997a88cb0a26182760cf2224a2ef2f
Thibault Duchateau <thibault.duchateau@gmail.com>
2015-05-20 13:06:19 +0200

    refactor(debugger): reorganize package
    Debug pages have been moved to the right location

--- datatables-core/src/main/java/com/github/dandelion/datatables/core/web/handler/debug/DatatableOptionGroupsDebugPage.java
+++ datatables-core/src/main/java/com/github/dandelion/datatables/core/web/handler/debug/DatatableOptionGroupsDebugPage.java
@@ -27,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.github.dandelion.datatables.web.handler.debug;
+package com.github.dandelion.datatables.core.web.handler.debug;
 
 import java.io.IOException;
 import java.util.ArrayList;
